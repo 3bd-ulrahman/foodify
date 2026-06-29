7ec9c104a214b1208e47cb98a3604bd85954c44f
3bd-ulrahman <abdulrahman198276301@gmail.com>
2026-06-29 23:10:41 +0300

    feat: design database erd

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 100);
             $table->string('phone', 20)->unique();
             $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
