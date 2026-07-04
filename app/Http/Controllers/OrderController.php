ac5d4324e01d4a1ffcdbe0bad34421b181e2408b
3bd-ulrahman <abdulrahman198276301@gmail.com>
2026-07-04 12:43:16 +0300

    feat: store order endpoint

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Actions\Order\CreateOrder;
+use App\Http\Requests\StoreOrderRequest;
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
 use App\Support\Pagination;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Symfony\Component\HttpFoundation\Response;
 
 class OrderController extends Controller
 {
@@ -22,5 +26,19 @@ class OrderController extends Controller
             ->paginate($request->integer('per_page', Pagination::DEFAULT_PER_PAGE));
 
         return OrderResource::collection($orders);
+    }
+
+    public function store(StoreOrderRequest $request, CreateOrder $action): JsonResponse
+    {
+        $order = $action->handle($request->validated(), auth()->user());
+
+        return OrderResource::make($order)
+            ->additional([
+                'meta' => [
+                    'message' => 'Order placed successfully.',
+                ],
+            ])
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
     }
 }
